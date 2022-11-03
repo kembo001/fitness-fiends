517ba3f7ed19a415c915adb9125ca8d1667d836e
Brandon <brandonkemboi87@gmail.com>
2022-11-03 14:52:59 -0500

    added firebase

---
+++ fitness-fiends/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDVeGafMSyzWoTIAKfr6LsX4XGQH0nEdaM",
+    authDomain: "fitness-fiends-31df9.firebaseapp.com",
+    projectId: "fitness-fiends-31df9",
+    storageBucket: "fitness-fiends-31df9.appspot.com",
+    messagingSenderId: "768583901727",
+    appId: "1:768583901727:web:c01c53611462566023ab2a",
+    measurementId: "G-NEPKJS59QW"
+  };
